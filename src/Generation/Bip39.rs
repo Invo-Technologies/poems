e190c639c67e271c7da5237bd40f698feff6b72a
DylanCKawalec <Dylankawalec@gmail.com>
2023-07-15 18:51:06 -0700

    newly created BIP39 Program

---
+++ src/Generation/Bip39.rs
@@ -0,0 +1,25 @@
+use bip39::{Mnemonic, Error};
+use rand::Rng;
+use hex;
+
+pub fn generate_entropy() -> Vec<u8> {
+    let mut rng = rand::thread_rng();
+    let mut entropy = vec![];
+
+    for _ in 0..32 {
+        let byte = rng.gen::<u8>();
+        entropy.push(byte);
+    }
+
+    entropy
+}
+
+pub fn hex_to_bin(hex_string: &str) -> Result<String, hex::FromHexError> {
+    let bytes = hex::decode(hex_string)?;
+    let bin = bytes.iter().fold(String::new(), |acc, b| acc + &format!("{:08b}", b));
+    Ok(bin)
+}
+
+pub fn hex_to_entropy(hex_string: &str) -> Result<Vec<u8>, hex::FromHexError> {
+    hex::decode(hex_string)
+}
---
+++ src/Generation/mod.rs
@@ -0,0 +1,4 @@
+pub mod aes;
+pub mod bip39;
+pub mod rsa;
+pub mod sha256;
